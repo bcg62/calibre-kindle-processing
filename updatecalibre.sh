5d7085aced65e45c87552c23f9bc73ca6082fd5e
Brendan Germain <brendan.germain@gmail.com>
2016-06-19 20:20:15 -0400

    added fetching new metadata

--- updatecalibre.sh
+++ updatecalibre.sh
@@ -6,9 +6,11 @@
 : ${LIBRARY:="/media/Books"}
 : ${CALIBRE_PATH:="/usr/local/bin"}
 : ${CACHE_FILE:="/var/cache/polished.books"}
-: ${CONVERT_FORMATS:="cbz cbr cbc chm epub fb2 lit lrf odt prc pdb pdf pml rb rtf snb tcr"}
+: ${CONVERT_FORMATS:="cbz cbr cbc chm epub fb2 lit lrf odt mobi prc pdb pdf pml rb rtf snb tcr"}
 : ${OUTPUT_FORMAT:="azw3"}
 : ${PROFILE:="kindle_pw"}
+
+[[ -f "$CACHE_FILE" ]] || touch "$CACHE_FILE"
 
 for ext in $CONVERT_FORMATS; do
 
@@ -16,7 +18,7 @@ for ext in $CONVERT_FORMATS; do
 
     [[ -z $file ]] && continue
 
-    output=${file%.*}.$OUTPUT_FORMAT
+    output="${file%.*}.$OUTPUT_FORMAT"
 
     [[ -f "$output" ]] &&
     {
@@ -24,7 +26,9 @@ for ext in $CONVERT_FORMATS; do
       continue
     }
 
-    $CALIBRE_PATH/ebook-convert "${file}" "${output}" --output-profile $PROFILE;
+    "$CALIBRE_PATH"/ebook-convert \
+    "${file}" "${output}" \
+    --output-profile $PROFILE;
 
   done <<<"$(find "$DOWNLOADS" -type f -name "*.$ext")"
 
@@ -34,19 +38,66 @@ done
 $(ls -1qA ${DOWNLOADS} | grep -q .) &&
 {
   echo "updating calibredb"
-  $CALIBRE_PATH/calibredb add --with-library=${LIBRARY} ${DOWNLOADS}
+  "$CALIBRE_PATH"/calibredb add --with-library=${LIBRARY} ${DOWNLOADS}
 
-  echo "embedding metadata"
-  $CALIBRE_PATH/calibredb embed_metadata all --with-library=${LIBRARY}
-
-  echo "polishing $OUTPUT_FORMAT books"
   while read -r file; do
 
-    [[ -f "$CACHE_FILE" ]] || touch "$CACHE_FILE"
+    # get calibre db id numebr
+    calibre_id=$(sed -n 's:.*d="calibre_id">\(.*\)<.*:\1:p' "${file%/*}/metadata.opf")
+
+    [[ -z $calibre_id ]] &&
+    {
+      echo "could not find calibre_id for $file"
+      continue
+    }
 
     # skip if ebook is in the cache and has been polished already
-    grep -q "$file" "$CACHE_FILE" ||
+    grep -q "^${calibre_id}$" "$CACHE_FILE" ||
     {
+
+      title=$(
+        "$CALIBRE_PATH"/calibredb \
+        show_metadata $calibre_id \
+        --with-library=${LIBRARY} |
+        sed -n 's/^Title[[:blank:]]*: \(.*\).*/\1/p'
+      )
+
+      authors=$(
+        "$CALIBRE_PATH"/calibredb \
+        show_metadata $calibre_id \
+        --with-library=${LIBRARY} |
+        sed -n 's/^Author(s)[[:blank:]]*: \(.*\).*/\1/p'
+      )
+
+      echo "fetching new metadata and cover for ${title}"
+      metadata=$(
+        "$CALIBRE_PATH"/fetch-ebook-metadata \
+        --title="$title" \
+        --authors="$authors" \
+        --cover "${file%/*}/cover.jpg" \
+        --opf
+      )
+
+      [[ $? -ne 0 ]] &&
+      {
+        echo "failed to fetch metadata for ${title}"
+        continue
+      }
+
+      echo "$metadata" > "${file%/*}/metadata.opf"
+
+      echo "updatding calibredb with new metadata"
+      "$CALIBRE_PATH"/calibredb \
+      set_metadata $calibre_id \
+      --with-library=${LIBRARY} \
+      "${file%/*}/metadata.opf"
+
+      echo "embedding new metadata into $file"
+      "$CALIBRE_PATH"/calibredb \
+      embed_metadata $calibre_id \
+      --with-library=${LIBRARY} \
+
+      echo "polishing $OUTPUT_FORMAT for ${title}"
       $CALIBRE_PATH/ebook-polish \
       --verbose \
       --cover "${file%/*}/cover.jpg" \
@@ -56,7 +107,7 @@ $(ls -1qA ${DOWNLOADS} | grep -q .) &&
       "$file" "$file"
 
       [[ $? -eq 0 ]] &&
-        echo "$file" >> "$CACHE_FILE"
+        echo "$calibre_id" >> "$CACHE_FILE"
     }
 
   done <<<"$(find "${LIBRARY}" -type f -name "*.$OUTPUT_FORMAT")"
